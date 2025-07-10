a440936249921ffdd4330db395fc4e922565ac12
rickypcyt <rickypcyt@gmail.com>
2025-07-11 00:48:29 +0200

    title remove

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,6 @@ const GlobeComponent = dynamic(() => import("./GlobeComponent"), { ssr: false })
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
-      <h1 className="text-3xl font-bold mb-8">Globo Terráqueo Interactivo</h1>
       <GlobeComponent />
     </div>
   );
